9bfb70069a7aa9f7c914e44d8a99ba1ddc9a1f32
Kirill Sysoev <k.sysoev@me.com>
2024-02-20 21:53:35 +0800

    Update dependencies and refactor Kafka producer

--- 03_wikimedia/producer.go
+++ 03_wikimedia/producer.go
@@ -1,49 +1,56 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
-	"log/slog"
+	"log"
+	"time"
 
+	"github.com/IBM/sarama"
 	"github.com/r3labs/sse/v2"
-	"github.com/segmentio/kafka-go"
 )
 
 func main() {
-	w := &kafka.Writer{
-		Addr:     kafka.TCP("localhost:9094", "localhost:9095", "localhost:9096"),
-		Topic:    "wikimedia_updates",
-		Balancer: &kafka.LeastBytes{},
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Idempotent = true
+	config.Net.MaxOpenRequests = 1
+	config.Producer.Retry.Max = 5
+	config.Producer.Retry.Backoff = 250 * time.Millisecond
+
+	config.Producer.Compression = sarama.CompressionSnappy
+	config.Producer.Flush.Frequency = 20 * time.Millisecond
+	config.Producer.Flush.Bytes = 32 * 1024
+
+	brokers := []string{"localhost:9094", "localhost:9095", "localhost:9096"}
+	producer, err := sarama.NewAsyncProducer(brokers, config)
+	if err != nil {
+		log.Panic(err)
 	}
 
 	client := sse.NewClient("https://stream.wikimedia.org/v2/stream/recentchange")
 
 	client.SubscribeRaw(func(msg *sse.Event) {
-		// By design of kafka-go it do batching of messages underhood and block until batch is ready
-		// So to avoid blocking we need to publish in goroutines
-		// alternativly we would need to batch on our own and reduce the batch timeout
+		var data map[string]interface{}
+
+		json.Unmarshal(msg.Data, &data)
+
+		var key string
+		key, ok := data["server_name"].(string)
+		if !ok {
+			key = "unknown"
+		}
+
+		message := &sarama.ProducerMessage{
+			Topic: "wikimedia_updates",
+			Key:   sarama.StringEncoder(key),
+			Value: sarama.ByteEncoder(msg.Data),
+		}
 		go func() {
-			var data map[string]any
-
-			json.Unmarshal(msg.Data, &data)
-
-			var key string
-			key, ok := data["server_name"].(string)
-			if !ok {
-				key = "unknown"
-			}
-
-			err := w.WriteMessages(context.Background(),
-				kafka.Message{
-					Key:   []byte(key),
-					Value: msg.Data,
-				},
-			)
-
-			if err != nil {
-				slog.Error("failed to write message:", err)
+			select {
+			case producer.Input() <- message:
+			case err := <-producer.Errors():
+				log.Println("Failed to produce message", err)
 			}
 		}()
 	})
-
 }
